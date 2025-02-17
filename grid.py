6079c96bad58a754ee88a6635fea8323ea14da7f
Mathew <44036272+Mathew-D@users.noreply.github.com>
2025-02-17 11:35:29 -0500

    Create grid.py

---
+++ grid.py
@@ -0,0 +1,15 @@
+//Made by: Mathew Dusome
+//Use to add grid.  Use the following:
+//  grid.gridHelp(window,WINDOW_WIDTH,WINDOW_HEIGHT)
+
+def gridHelp(window,WINDOW_WIDTH,WINDOW_HEIGHT):
+        spacer = 10
+        font = pygame.font.SysFont('Consolas', 10)
+        for gridX in range(0, WINDOW_WIDTH, spacer):        
+            window.blit(pygame.transform.rotate(font.render(str(gridX), True, (0, 0, 0)),90),(gridX,0))
+        for gridY in range(0, WINDOW_HEIGHT, spacer):
+            window.blit(font.render(str(gridY), True, (0, 0, 0)), (0, gridY))
+        for gridX in range(0, WINDOW_WIDTH, spacer):
+            pygame.draw.line(window,(255,0,0),(gridX,0),(gridX,WINDOW_HEIGHT))
+        for gridY in range(0, WINDOW_HEIGHT, spacer):
+            pygame.draw.line(window,(255,0,0),(0,gridY),(WINDOW_WIDTH,gridY))     
